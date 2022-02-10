5f1874f249eda09a56a3072326c1c9bd96f857ac
= <=>
2022-02-10 07:48:40 -0600

    added schemas boilerplate

--- server/models/Clinician.js
+++ server/models/Clinician.js
@@ -1,2 +1,54 @@
-const {Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt')
+
+const clinicianSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must match an email address!']
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8
+        },
+        patients: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'patient'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+clinicianSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// compare the incoming password with the hashed password
+clinicianSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+
+const User = model('User', userSchema);
+
+module.exports = User;
